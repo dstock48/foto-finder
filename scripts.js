db10619d19f015fb310ad655e53a15e810873d05
Juan Diego Jimenez <jdiejim@gmail.com>
2017-03-28 13:07:05 -0600

    add file path functionality

--- scripts.js
+++ scripts.js
@@ -6,18 +6,16 @@ function Foto(title, caption, path) {
   this.caption = caption;
   this.path = path;
 }
-// Main
-// Functions
+
 // Events
 document.getElementById('save-button').addEventListener('click', function() {
   // DOM Element values
   var title = document.getElementById('input-title').value;
   var caption = document.getElementById('input-caption').value;
-  // TODO: Get path from button
-  var path = document.getElementById('file').files[0];
+  var pathUrl = "photos/" + document.getElementById('file').files[0].name;
 
   // Create foto object
-  var foto = new Foto(title ,caption , path);
+  var foto = new Foto(title, caption, pathUrl);
 
   // Create Card Element
   var cardContainer = document.getElementById('card-container');
@@ -33,20 +31,20 @@ document.getElementById('save-button').addEventListener('click', function() {
   // Add Data to DOM
   cardHeader.innerText = foto.title;
   cardCaption.innerText = foto.caption;
-  cardImg.setAttribute('src', 'photos/photo1.png');
+  cardImg.setAttribute('src', foto.path);
   deleteIcon.setAttribute('src', 'assets/delete.svg');
   favoriteIcon.setAttribute('src', 'assets/favorite.svg');
 
-  //Assing ID
+  //Assign ID to Elements
   deleteIcon.setAttribute('id', 'delete');
   favoriteIcon.setAttribute('id', 'favorite');
 
-  // Assign Class to Element
+  // Assign Class to Elements
   card.className = 'card';
   cardImg.className = 'card-image';
   iconContainer.className = 'icons';
 
-  // Append to Card Element
+  // Append to Card Elements
   card.appendChild(cardHeader);
   card.appendChild(cardFigure);
   cardFigure.appendChild(cardImg);
@@ -55,13 +53,9 @@ document.getElementById('save-button').addEventListener('click', function() {
   iconContainer.appendChild(deleteIcon);
   iconContainer.appendChild(favoriteIcon);
 
-  // Append to Body
+  // Append to Card Container
   cardContainer.appendChild(card);
   fotos.push(foto);
+
+  // TODO: clear input after button pressed
 });
-
-
-// <div class="icons">
-//   <img id="delete" src="assets/delete.svg" alt="">  <!-- BUG: these can't be ID's because there will be many of them on the page -->
-//   <img id="favorite" src="assets/favorite.svg" alt="">  <!-------->
-// </div>
