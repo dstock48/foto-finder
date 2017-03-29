d75deffb400d53e1c86064628950019994a3c569
Dave W <dviper@gmail.com>
2017-03-28 22:22:43 -0600

    Add input validation functionality Disable 'Add To Album' button by default Add clear input functionality after photo submission

--- scripts.js
+++ scripts.js
@@ -1,5 +1,7 @@
 // Globals
 var favorites = [];
+var deleteBtn = document.getElementById('delete');
+var cardContainer = document.getElementById('card-container');
 // Classes
 function Foto(title, caption, path) {
   this.title = title;
@@ -7,12 +9,32 @@ function Foto(title, caption, path) {
   this.path = path;
 }
 
+function validateInput(e) {
+  // console.log(e);
+  var title = document.getElementById('input-title').value;
+  var caption = document.getElementById('input-caption').value;
+  var file = document.getElementById('file').files[0];
+
+  if (title !== "" && caption !== "" && file !== undefined) {
+    document.getElementById('save-button').removeAttribute("disabled", "");
+  } else {
+    document.getElementById('save-button').setAttribute("disabled", "");
+  }
+}
+
 // Events
+document.getElementById('input-title').addEventListener('input', validateInput);
+document.getElementById('input-caption').addEventListener('input', validateInput);
+document.getElementById('file').addEventListener('change', validateInput);
 document.getElementById('save-button').addEventListener('click', function() {
   // DOM Element values
+
   var title = document.getElementById('input-title').value;
   var caption = document.getElementById('input-caption').value;
   var pathURL = "photos/" + document.getElementById('file').files[0].name;
+  var titleInput = document.getElementById('input-title');
+  var captionInput = document.getElementById('input-caption');
+  var fileInput = document.getElementById('file');
 
   // Create foto object
   var foto = new Foto(title, caption, pathURL);
@@ -57,24 +79,13 @@ document.getElementById('save-button').addEventListener('click', function() {
 
   // Append to Card Container
   cardContainer.appendChild(card);
-
-
-  // TODO: clear input after button pressed
-
+  document.getElementById('save-button').setAttribute("disabled", "");
+  document.getElementById('file').value = '';
+  titleInput.value = '';
+  captionInput.value = '';
 });
 
-// TODO: Delete icon functional
-var deleteBtn = document.getElementById('delete');
-var cardContainer = document.getElementById('card-container');
-
-
-
-
-
-
 cardContainer.addEventListener('click', function(e) {
-  // console.log(e.target.parentElement.parentElement);
-  // console.log(e);
   var idAttribute = e.target.id;
   var thisCard = e.target.parentElement.parentElement;
   var favoriteIcon = e.target;
@@ -98,9 +109,6 @@ cardContainer.addEventListener('click', function(e) {
         console.log(favorites);
         break;
       default:
-
     }
   }
-
-  // cardContainer.removeChild(card);
 });
